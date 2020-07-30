4e000104792b4d936cf0a57b13fc0ec790335883
Angela Ng <an378@cornell.edu>
2020-07-30 14:48:28 -0400

    Initial Commit

---
+++ gizou-hin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gizou-hin
+//
+//  Created by Angela Ng on 7/30/20.
+//  Copyright © 2020 Angela Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
